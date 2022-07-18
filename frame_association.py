d22adc81ded861274fb010cd90e2bbdcec268373
Hu Mingzhe <hmz_superb@qq.com>
2022-07-18 00:24:17 -0400

    quick debug

--- frame_association.py
+++ frame_association.py
@@ -6,16 +6,19 @@ Date: Jul 16, 2022
 """
 import numpy as np
 from collections import defaultdict
-from sklearn.cluster import DBSCAN
+try:
+    from cuml import DBSCAN
+except ImportError:
+    from sklearn.cluster import DBSCAN
 from scipy.optimize import linear_sum_assignment
 from scipy.spatial.distance import cdist, pdist
 
 PRECISION = np.float16
 
 
-def cal_dist(left_matrix: np.ndarray, right_matrix: np.ndarray, weight=0.9):
-    if (left_matrix == right_matrix).all():
-        return pdist(left_matrix, "euclidean") * weight + pdist(right_matrix, "cosine") * (1-weight)
+def cal_dist(left_matrix: np.ndarray or list, right_matrix=None, weight=0.9):
+    if right_matrix is None:
+        return pdist(left_matrix, "euclidean") * weight + pdist(left_matrix, "cosine") * (1-weight)
     return cdist(left_matrix, right_matrix, "euclidean") * weight + cdist(left_matrix, right_matrix, "cosine") * (1-weight)
 
 
@@ -30,7 +33,7 @@ def get_camera_bias(frames):
     bias = dict()
     for sensor in embeddings_under_sensor:
         embeddings = np.asarray(embeddings_under_sensor[sensor], dtype=PRECISION)
-        dist = cal_dist(embeddings, embeddings)
+        dist = cal_dist(embeddings)
         # Exclude noise
         clustering_labels = DBSCAN(eps=0.5, min_samples=10, metric="precomputed").fit(dist).labels_
         valid_index = []
@@ -78,37 +81,38 @@ def frame_level_matching(frames,
         else:
             while len(prev) < len(embeddings):
                 prev = np.append(prev, np.asarray([[3. for _ in range(embedding_dim)]], dtype=PRECISION), axis=0)
-            dist = cal_dist(prev, np.asarray(embeddings))
-            row_ind, col_ind = linear_sum_assignment(dist)
-            del dist
-            sorted_indices = np.argsort(col_ind)
-            assignment = row_ind[sorted_indices]
-            # Previous embeddings pending for update
-            for i, assign in enumerate(assignment):
-                # Notation i means detection of the current frame;
-                # Notation assign means corresponding detection of the previous frame
-                if i == assign:
-                    continue
-                if behavior_length[assign]:
-                    prev_embedding = (1 - momentum) * prev[assign] + \
-                                     momentum * behavior_embeddings[assign] / behavior_length[assign]
-                else:
-                    prev_embedding = prev[assign]
-                cur_embedding = embeddings[i]
-                pair_dist = cal_dist(prev_embedding, cur_embedding)[0][0]
-                if pair_dist > distance_threshold:
-                    # Assign it to a new id
-                    prev = np.append(prev, np.asarray([cur_embedding], dtype=PRECISION), axis=0)
-            del assignment
-            # re-associate
-            cost = cal_dist(prev, np.asarray(embeddings, dtype=PRECISION))
-            row_ind, col_ind = linear_sum_assignment(cost)
-            del cost
-            sorted_indices = np.argsort(col_ind)
-            assignment = row_ind[sorted_indices]
-            for i, assign in enumerate(assignment):
-                labels[frame_id + "@" + sensor_id][local_ids[i]] = assign
-                behavior_embeddings[assign] += np.asarray(embeddings[i], dtype=PRECISION)
-                behavior_length[assign] += 1
-                prev[assign] = embeddings[i]
+            if embeddings:
+                dist = cal_dist(prev, np.asarray(embeddings))
+                row_ind, col_ind = linear_sum_assignment(dist)
+                del dist
+                sorted_indices = np.argsort(col_ind)
+                assignment = row_ind[sorted_indices]
+                # Previous embeddings pending for update
+                for i, assign in enumerate(assignment):
+                    # Notation i means detection of the current frame;
+                    # Notation assign means corresponding detection of the previous frame
+                    if i == assign:
+                        continue
+                    if behavior_length[assign]:
+                        prev_embedding = (1 - momentum) * prev[assign] + \
+                                         momentum * behavior_embeddings[assign] / behavior_length[assign]
+                    else:
+                        prev_embedding = prev[assign]
+                    cur_embedding = embeddings[i]
+                    pair_dist = cal_dist([prev_embedding], [cur_embedding])[0][0]
+                    if pair_dist > distance_threshold:
+                        # Assign it to a new id
+                        prev = np.append(prev, np.asarray([cur_embedding], dtype=PRECISION), axis=0)
+                del assignment
+                # re-associate
+                cost = cal_dist(prev, np.asarray(embeddings, dtype=PRECISION))
+                row_ind, col_ind = linear_sum_assignment(cost)
+                del cost
+                sorted_indices = np.argsort(col_ind)
+                assignment = row_ind[sorted_indices]
+                for i, assign in enumerate(assignment):
+                    labels[frame_id + "@" + sensor_id][local_ids[i]] = assign
+                    behavior_embeddings[assign] += np.asarray(embeddings[i], dtype=PRECISION)
+                    behavior_length[assign] += 1
+                    prev[assign] = embeddings[i]
     return labels
