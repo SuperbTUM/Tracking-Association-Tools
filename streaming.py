6736a136dbb724224775f2574c7ef83ecd54743d
Hu Mingzhe <hmz_superb@qq.com>
2022-10-11 10:49:05 -0400

    modify kafka streaming

--- streaming.py
+++ streaming.py
@@ -10,17 +10,29 @@ def init_producer():
                          value_serializer=lambda v: json.dumps(v).encode())
 
 
-def init_consumer(topic, timeout):
-    return KafkaConsumer(topic=topic,
-                         bootstrap_servers="localhost:9092",
-                         group_id=None,
-                         auto_offset_reset="earliest",
-                         enable_auto_comit=False,
-                         consumer_timeout_ms=timeout)
+def init_consumer(topic, timeout=1000):
+    consumer = KafkaConsumer(bootstrap_servers="localhost:9092",
+                             group_id=None,
+                             auto_offset_reset="earliest",
+                             enable_auto_comit=False,
+                             consumer_timeout_ms=timeout)
+    consumer.subscribe(topic)
+    return consumer
 
 
 def produce_record(producer, topic, data, partition):
-    return producer.send(topic=topic, partition=partition, value=data)
+    """data could be a few lines"""
+
+    def callback(err, msg):
+        if err:
+            print("%% Msg failed delivery: %s \n", err)
+        else:
+            print("%% Msg delivered to %s [%d] \n" % (msg.topic(), msg.partition()))
+    for line in data:
+        producer.send(topic=topic, partition=partition, value=line, callback=callback)
+        producer.poll(1.0)
+        partition += 1
+    producer.flush()
 
 
 def consume_record(consumer):
@@ -30,3 +42,17 @@ def consume_record(consumer):
         rec_list.append(ast.literal_eval(r))
         consumer.commit()
     return rec_list
+
+
+if __name__ == "__main__":
+    topic_data = dict()
+    producer1 = init_producer()
+    topic1 = ("news", )
+    data1 = ["Columbia University", "University of California"]
+    produce_record(producer1, topic1[0], data1, 0)
+    consumer1 = init_consumer(topic1, 1000)
+    consumer2 = init_consumer(topic1, 1000)
+    consume_record(consumer1)
+    data2 = ["CNN", "NBC News"]
+    produce_record(producer1, topic1[0], data2, 0)
+    consume_record(consumer2)
